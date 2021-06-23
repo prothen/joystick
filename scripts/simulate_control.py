a25811c8d0f0e7d4a6c7f38cd1112dd6e1b5f08f
Philipp Rothenhäusler <phirot@kth.se>
2021-06-23 14:18:02 +0200

    Add haptic feedback simulator
    * Add haptic feedback publisher at fixed frequency with republished
    controls
    
    * Update launch file for remapping with absolute namespacing
    
    * Use joystick_msgs HapticControlStamped with EXTERNAL_FORCE mode

--- scripts/simulate_control.py
+++ scripts/simulate_control.py
@@ -11,13 +11,15 @@ __email__ = "philipp.rothenhaeusler@gmail.com"
 
 import attr
 import rospy
+import numpy
 import typing
 import sensor_msgs.msg
 import geometry_msgs.msg
+import joystick_msgs.msg
 
 
 @attr.s
-class ControlSimulator
+class ControlSimulator:
     """ Conversion node from joystick to alternative control topic.
 
         Note:
@@ -28,24 +30,32 @@ class ControlSimulator
             to two dimensional XY joystick applications.
 
     """
-
+    # ROS publishers
     _publisher_control = attr.ib(default=None,
             type=typing.Optional[rospy.Publisher])
 
+    _publisher_haptic_feedback = attr.ib(default=None,
+            type=typing.Optional[rospy.Publisher])
+
+    # ROS messages
     _msg_control = attr.ib(default=None,
             type=typing.Optional[geometry_msgs.msg.Vector3Stamped])
 
-    _last_control_stamp = attr.ib(default=None,
-            type=typing.Optional[rospy.Time])
+    _msg_haptic_feedback = attr.ib(default=None,
+            type=typing.Optional[joystick_msgs.msg.HapticControlStamped])
 
     # Upper bound for republishing frequency
     _frequency = attr.ib(default=100, type=float)
     _rate = attr.ib(default=None, type=typing.Optional[rospy.Rate])
 
+    # Deadzone to consider zero input _d : [-th, th] -> 0
+    _deadzone_threshold = attr.ib(default=.2, type=float)
+
     def __attrs_post_init__(self):
         rospy.init_node("placeholder")
         rospy.Subscriber("~joystick_input",
-                sensor_msgs.msg.Joy)
+                sensor_msgs.msg.Joy,
+                self._cb_joystick_input)
 
         self._publisher_control = rospy.Publisher(
             "~control",
@@ -53,20 +63,42 @@ class ControlSimulator
             queue_size=1,
             tcp_nodelay=True)
 
+        self._publisher_haptic_feedback = rospy.Publisher(
+            "~haptic_feedback",
+            joystick_msgs.msg.HapticControlStamped,
+            queue_size=1,
+            tcp_nodelay=True)
+
         c = geometry_msgs.msg.Vector3Stamped()
         self._msg_control = c
 
+        c = joystick_msgs.msg.HapticControlStamped()
+        c.mode = joystick_msgs.msg.HapticControlStamped.EXTERNAL_FORCE
+        self._msg_haptic_feedback = c
+
         self._rate = rospy.Rate(self._frequency)
+
+    def _deadzone(self, value):
+        """ Apply joystick deadzone. """
+        return 0 if numpy.abs(value) < self._deadzone_threshold else value
 
     def _cb_joystick_input(self, msg: sensor_msgs.msg.Joy):
         """ Parse joystick input with fixed mapping. """
         self._msg_control.header.stamp = msg.header.stamp
-        self._msg_control.vector.x = msg.axes[1]
-        self._msg_control.vector.y = msg.axes[0]
+        self._msg_control.vector.x = self._deadzone(msg.axes[1])
+        self._msg_control.vector.y = self._deadzone(msg.axes[0])
+
+        # Apply joystick input as haptic feedback output
+        self._msg_haptic_feedback.stamp = msg.header.stamp
+        self._msg_haptic_feedback.control = self._msg_control.vector
 
     def _publish_control(self):
         """ Publish parsed control message. """
         self._publisher_control.publish(self._msg_control)
+
+    def _publish_haptic_feedback(self):
+        """ Publish parsed control message. """
+        self._publisher_haptic_feedback.publish(self._msg_haptic_feedback)
 
     def run(self):
         """ Execute main loop. """
@@ -81,7 +113,8 @@ class ControlSimulator
 
         while not rospy.is_shutdown():
             self._publish_control()
-            self_rate.sleep()
+            self._publish_haptic_feedback()
+            self._rate.sleep()
 
 
 if __name__ == "__main__":
